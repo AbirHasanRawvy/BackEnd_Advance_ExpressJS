ed1fa08f519cdb694bbd8d61a0c176770eb843b9
Abir Hasan <abirhasanrawvy@gmail.com>
2026-07-01 02:41:40 +0600

    Add files via upload

--- middleWare.js
+++ middleWare.js
@@ -1,3 +1,32 @@
 ///req -> the req is going to client.
 ///res -> the res coming to server.
 /// next -> going to middle ware or root.
+
+const express = require("express");
+const app = express();
+
+//simple middleware
+const logger = (req, res, next)=>{
+    console.log("Request received at: ", new Data().toISOString() );
+    next();
+}
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+    res.json({
+        name: "Alex",
+        age: 25,
+        profession: "Full Stack Developer",
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server is ryning!");
+});
+
+//Application level middleware
+//Route level
+//Error handling
+//Built in
+//Third party 
