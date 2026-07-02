dbf43e3f595fac5f6fe36a0dd0a92334b1d52de3
Abir Hasan <abirhasanrawvy@gmail.com>
2026-07-03 00:50:03 +0600

    Working With Multipart Form Data (File Upload)

--- multiPartFromData.js
+++ multiPartFromData.js
@@ -1,1 +1,31 @@
 ///Working With Multipart Form Data (File Upload)
+const express = require("express");
+const multer = require("multer");
+
+const app = express();
+
+
+//multer storage configaration;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')//where is my file will be save.
+ },
+
+ filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniqueSuffix + "_" + file.originalname);
+  },
+});
+
+const upload = multer({storage:storage});
+
+app.post("/upload", upload.single("file"), (req, res)=>{
+    res.send(`File upload successfully:  ${req.file.filename}`);
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
+
+console.log(Date.now() + '-' + Math.round(Math.random() * 1E9));
