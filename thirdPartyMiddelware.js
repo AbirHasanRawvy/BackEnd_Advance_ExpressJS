fdd64e1493f7c8541c8d6bbcedadb549861f76cd
Abir Hasan <abirhasanrawvy@gmail.com>
2026-07-06 01:00:21 +0600

    Third Party Middelware.

--- thirdPartyMiddelware.js
+++ thirdPartyMiddelware.js
@@ -2,6 +2,12 @@
 ///Exress rate limit - npm i express-rate-limit.-Its a dos attack solution.
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");//Helmet: its come to hacking;
+const mongoSanitize = require("express-mongo-sanitize");//data safety;
+const hpp = require("hpp");//refress all the things;
+const cors = require("cors");
+const cookieParser = require("cookie-parser");//when we work with cookies;
+const morgan = require("morgan");//collect all the information;
 const app = express();
 
 
@@ -12,7 +18,18 @@ const limiter = rateLimit({
 
 })
 
+app.use(limiter);
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(hpp());
+app.use(cors());
+app.use(cookieParser());
+app.use()
+app.use(margan("dev"));
+
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
 //Dos  attack - Over request on the server;
+
