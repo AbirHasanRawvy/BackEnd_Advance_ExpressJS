d01586fc3875aad26d85236f1ee86b7fd56989d3
Abir Hasan <abirhasanrawvy@gmail.com>
2026-07-13 02:57:51 +0600

    Create express js rest api project part-1.

--- app.js
+++ app.js
@@ -3,3 +3,40 @@ const express = require("express");
 const app = new express();
 
 module.exports = app;
+
+//Security Middleware;
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+
+
+
+//! ENV
+const dotENV = require('dotenv');
+dotENV.config();
+
+
+//!Seciruty Middleware implement
+app.use(helmet());
+app.use(xss());
+app.use(hpp());
+app.use(cors());
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({ limit: "100md" }));
+
+//! Rate Limit
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3000,
+});
+app.use(limiter);
+
+
+//!data base work;
+
+
+
+
+ module.exports = app;
