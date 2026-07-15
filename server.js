983044780fd899e14249adb3620b33ade1f47c45
Abir Hasan <abirhasanrawvy@gmail.com>
2026-07-16 00:38:50 +0600

    Express.Js Request, Query, Multipart, Middleware.

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 //!
 import express from "express";
+import multer from "multer"; 
+import path from "path";
+
 
 const app = express();
 const PORT = 5000;
+
+//! Multer setup for file uploading;
+const storage = multer.diskStorage({
+    destination: (req, File, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer ({ storage });
+
+//!
 
 app.get("/", (req, res) => {
 
